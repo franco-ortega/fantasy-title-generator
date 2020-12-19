6c6523881f72fd30ee6517cb816b518cfdf9fa48
Franco Ortega <francopdx@gmail.com>
2020-12-19 15:45:28 -0800

    add test for getWords() to return strings

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -1,68 +1,47 @@
 const { test } = QUnit;
 //import { add, addThree, addString } from './sample-tests.js';
 import { adjectiveGroups, nounGroups, adjectiveOrNounGroup } from '../data/data.js';
+import { getWords } from '../js/utils.js'
 
 // Word selection tests
 QUnit.module('Word Selection Tests');
 
-test('should return two different adjectives', function(assert) {
+  test('should return two different adjectives', function(assert) {
 
-  const adjectives = getWords(adjectiveGroups);
+    const adjectives = getWords(adjectiveGroups);
 
-  assert.notEqual(adjectives[0], adjectives[1]);
-});
+    assert.notEqual(adjectives[0], adjectives[1]);
+  });
 
-test('should return two different nouns', function(assert) {
+  test('should return two different nouns', function(assert) {
 
-  const nouns = getWords(nounGroups);
+    const nouns = getWords(nounGroups);
 
-  assert.notEqual(nouns[0], nouns[1]);
-});
+    assert.notEqual(nouns[0], nouns[1]);
+  });
 
-test('should return two different adjectives or nouns', function(assert) {
+  test('should return two different adjectives or nouns', function(assert) {
 
-  const adjectivesOrNouns = getWords(adjectiveOrNounGroup);
+    const adjectivesOrNouns = getWords(adjectiveOrNounGroup);
 
-  assert.notEqual(adjectivesOrNouns[0], adjectivesOrNouns[1]);
-});
+    assert.notEqual(adjectivesOrNouns[0], adjectivesOrNouns[1]);
+  });
 
+  test('should return a string', function(assert) {
 
+    const words = getWords(adjectiveGroups);
+    const wordOne = words[0];
+    const wordTwo = words[1];
 
-// First set of sample tests
-
-
-// QUnit.module('add', () => {
-//   test('should add two numbers', function(assert) {
-//       assert.equal(add(1, 1), 2, '1 + 1 = 2');
-//   });    
-
-//   test('should add two numbers', function(assert) {
-//       const numOne = 5;
-//       const numTwo = 6;
-//       const expected = 11;
-
-//       const actual = add(numOne, numTwo);
-
-//       assert.equal(actual, expected, '5 + 6 = 11');
-//   });    
-
-//   test('should add two neagtive numbers', function(assert) {
-
-//     const numOne = -1;
-//     const numTwo = -2;
-//     const expected = -3;
-
-//     const actual = add(numOne, numTwo);
-
-//       assert.strictEqual(actual, expected, `${numOne} + ${numTwo} = ${expected}`);
-//   });    
-
-//   QUnit.module('addThree', function() {
-//     test('should add three numbers', function(assert) {
-//       assert.equal(addThree(1, 1, 1), 3, '1 + 1 + 1 = 3');
-//     });  
-//   });  
-
-// });  
-
+    let trueOrFalseOne;
+    if(typeof wordOne === 'string') { trueOrFalseOne = true; }
+    else { trueOrFalseOne = false; };
+    
+    let trueOrFalseTwo;
+    if(typeof wordTwo === 'string') { trueOrFalseTwo = true; }
+    else { trueOrFalseTwo = false; };
+    
+    assert.true(trueOrFalseOne);
+    assert.true(trueOrFalseTwo);
+  });
 
