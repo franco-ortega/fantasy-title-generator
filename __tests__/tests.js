59c89fc26b645b7bf1a45c10387385c4d025e55b
Franco Ortega <francopdx@gmail.com>
2020-12-19 15:14:12 -0800

    add tests for getWords() to select two different words

--- __tests__/tests.js
+++ __tests__/tests.js
@@ -1,54 +1,68 @@
 const { test } = QUnit;
-import { add, addThree, addString } from './sample-tests.js';
+//import { add, addThree, addString } from './sample-tests.js';
 import { adjectiveGroups, nounGroups, adjectiveOrNounGroup } from '../data/data.js';
 
-const test1 = adjectiveGroups;
-const test2 = nounGroups;
-const test3 = adjectiveOrNounGroup;
-console.log(test1, test2, test3);
+// Word selection tests
+QUnit.module('Word Selection Tests');
+
+test('should return two different adjectives', function(assert) {
+
+  const adjectives = getWords(adjectiveGroups);
+
+  assert.notEqual(adjectives[0], adjectives[1]);
+});
+
+test('should return two different nouns', function(assert) {
+
+  const nouns = getWords(nounGroups);
+
+  assert.notEqual(nouns[0], nouns[1]);
+});
+
+test('should return two different adjectives or nouns', function(assert) {
+
+  const adjectivesOrNouns = getWords(adjectiveOrNounGroup);
+
+  assert.notEqual(adjectivesOrNouns[0], adjectivesOrNouns[1]);
+});
+
+
 
 // First set of sample tests
-QUnit.module('add', () => {
-  test('should add two numbers', function(assert) {
-      assert.equal(add(1, 1), 2, '1 + 1 = 2');
-  });
 
-  test('should add two numbers', function(assert) {
-      const numOne = 5;
-      const numTwo = 6;
-      const expected = 11;
 
-      const actual = add(numOne, numTwo);
+// QUnit.module('add', () => {
+//   test('should add two numbers', function(assert) {
+//       assert.equal(add(1, 1), 2, '1 + 1 = 2');
+//   });    
 
-      assert.equal(actual, expected, '5 + 6 = 11');
-  });
+//   test('should add two numbers', function(assert) {
+//       const numOne = 5;
+//       const numTwo = 6;
+//       const expected = 11;
 
-  test('should add two neagtive numbers', function(assert) {
+//       const actual = add(numOne, numTwo);
 
-    const numOne = -1;
-    const numTwo = -2;
-    const expected = -3;
+//       assert.equal(actual, expected, '5 + 6 = 11');
+//   });    
 
-    const actual = add(numOne, numTwo);
+//   test('should add two neagtive numbers', function(assert) {
 
-      assert.strictEqual(actual, expected, `${numOne} + ${numTwo} = ${expected}`);
-  });
+//     const numOne = -1;
+//     const numTwo = -2;
+//     const expected = -3;
 
-  QUnit.module('addThree', function() {
-    test('should add three numbers', function(assert) {
-      assert.equal(addThree(1, 1, 1), 3, '1 + 1 + 1 = 3');
-    });
-  });
+//     const actual = add(numOne, numTwo);
 
-});
+//       assert.strictEqual(actual, expected, `${numOne} + ${numTwo} = ${expected}`);
+//   });    
 
-// Second suite of sample tests
-QUnit.module('String Tests');
+//   QUnit.module('addThree', function() {
+//     test('should add three numbers', function(assert) {
+//       assert.equal(addThree(1, 1, 1), 3, '1 + 1 + 1 = 3');
+//     });  
+//   });  
 
-test('should add two strings', function(assert) {
-  assert.equal(addString('Hello', 'there'), 'Hellothere', 'Hellothere');
-});
+// });  
 
-test('should add two strings', function(assert) {
-  assert.equal(addString('Hey', 'yo'), 'Heyyo', 'Heyyo');
-});
+
---
+++ js/utils.js
@@ -0,0 +1,30 @@
+// FUNCTIONS
+
+// WORD SELECTION
+// Randomly select one list (array of strings) from the collection (array of lists). Then, randomly select one word(string) from the list (array).
+// Two adjectives will be selected this way from the Adjective Collection. Both adjectives can come from the same list. But each adjective must be different.
+// Two nouns will be selected the same way with the same restrictions but from the Noun Collection.
+//
+// How to do this:
+// 1. Count the length of the Adj Collection array
+    // 2a. Use Math.random to select one Adj List from the Adj Collection
+        // 2b. Count the length of the Adj List array
+        // 2c. Use Math.random to select one Adjective from the Adj List
+        // 2d. Return the selected Adjective (2c)
+    // 3a. (same as 2a)
+        // 3b. (same as 2b)
+        // 3c. (same as 2c)
+        // 3d. Check if 3c is the same as 2c
+            // 3d-1. If same, repeat step 3
+            // 3d-2. If different, return the Adjective (3c)
+// 4. Result: two different Adjectives (from the same or different Lists)
+// 5. Repeat this process for the Noun Collection to select two different Nouns
+//
+// How many possible functions?
+// 1. Count length of array (Collections and Lists): number = array.length
+// 2. Get random index/item from array (List and Word): Math.floor(math.random() * number)
+// 3. Check to see if two strings are different (Word vs Word): if(wordOne === wordTwo)
+//
+// How to test?
+// Check to see if two strings have been selected
+// Check to see if two strings are not equal
