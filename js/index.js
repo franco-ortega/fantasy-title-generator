167f752a65eec5c5fc6ce53d3aeaaca34b03ab8f
Franco Ortega <francopdx@gmail.com>
2020-12-22 22:59:56 -0800

    clean up code for fade effects; add additional Title pages to get feedback for different stylings

--- js/index.js
+++ js/index.js
@@ -1,33 +1,28 @@
-import { adjectiveLists, nounLists, adjectiveOrNounList } from '../data/data.js';
+import { adjectiveLists, nounLists } from '../data/data.js';
 import { getTwoWords, createTitle, saveTitle } from '../js/utils.js'
 import { TITLES } from './constants.js';
 
-const titleSpan = document.querySelector('.span-title');
+const titleDisplayCase = document.querySelector('.title-display');
 const titleButton = document.querySelector('.button-title');
 const nameInput = document.querySelector('.input-name');
 
-titleButton.addEventListener('click', () => {
-    const oldDiv = document.querySelector('.div-title');
-    if(oldDiv) {
-        oldDiv.remove();
+titleButton.addEventListener('click', () => {    
+    const oldContainer = document.querySelector('.span-title');
+    if(oldContainer) {
+        oldContainer.remove();
     };
     
-    const spanDiv = document.createElement('div');
-    spanDiv.classList.add('div-title');
-
+    const titleContainer = document.createElement('span');
+    titleContainer.classList.add('span-title');
+    
     const name = nameInput.value;
-
     const adjectives = getTwoWords(adjectiveLists);
-
     const nouns = getTwoWords(nounLists);
-
     const title = createTitle(name, adjectives, nouns);
 
     saveTitle(title, TITLES);
 
-    // titleSpan.textContent = title;
-    spanDiv.classList.add('fade-in');
-    // spanDiv.classList.add('fade-in-down');
-    spanDiv.textContent = title;
-    titleSpan.appendChild(spanDiv);
+    titleContainer.classList.add('fade-in');
+    titleContainer.textContent = title;
+    titleDisplayCase.appendChild(titleContainer);
 });
--- js/titles.js
+++ js/titles.js
@@ -2,22 +2,19 @@ import { getTitles } from './utils.js';
 import { TITLES } from './constants.js';
 
 const displayCase = document.getElementById('display-case');
+// const container = document.createElement('div');
+// displayCase.appendChild(container);
 
 const allTitles = getTitles(TITLES);
 
 for(let i = 0; i < allTitles.length; i++) {
-    const container = document.createElement('div');
-    displayCase.appendChild(container);
-
-    setTimeout(() => {
     const oneTitle = allTitles[i];
-    
+
+    setTimeout(() => {    
         const titleHolder = document.createElement('div');
         titleHolder.classList.add('fade-in');
-        titleHolder.classList.add('fade-in-down');
-        titleHolder.textContent = `* ${oneTitle} *`;
-        container.appendChild(titleHolder);
-
+        // titleHolder.classList.add('fade-in-down');
+        titleHolder.textContent = `*${oneTitle}*`;
+        displayCase.appendChild(titleHolder);
     }, 200 * i);
-
 };
